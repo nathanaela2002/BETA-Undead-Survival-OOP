6ddfb8e080dd4c71d8bfae04862c3b8f2ba56857
ajerlkjdflkajfkldjflkfkjdsklfjskldfjsldkjfksldfjkldsfjldsksjldkfjlk <60791237+smauxy@users.noreply.github.com>
2022-06-15 23:12:18 -0400

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,1 +1,36 @@
-
+ 
+import java.util.LinkedList;
+import java.util.Queue;
+ // first base code of a queue just gonna change how the item is added to queue and how the power is given
+public class QueueExample {
+ 
+    public static void main(String[] args)
+    {
+        Queue<Integer> q
+            = new LinkedList<>();
+ 
+        // Adds items to the queue from pickup figuring this out
+        // idk how to make it so when u pickup it get s the item tbh
+ 
+        // Display contents of the inventory queue.
+        System.out.println("inventroy"
+                           + q);
+ 
+        // To use the next item.
+        int removedele = q.remove();
+        System.out.println("used item-"
+                           + removedele);
+      //gonan throw a if statemtn to when head of queue is removed we gain power or whatever the item is ill find this
+ 
+        System.out.println(q);
+ 
+        // To view the next item
+        int head = q.peek();
+        System.out.println("next item-"
+                           + head);
+ 
+        int size = q.size();
+        System.out.println("total inv space-"
+                           + size);
+    }
+}
